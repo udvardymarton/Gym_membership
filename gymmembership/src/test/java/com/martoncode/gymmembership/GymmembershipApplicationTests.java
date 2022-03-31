ef6283c1908e3e22b698c563f0732d986ae1ca79
udvardymarton <martonudvardy4@gmail.com>
2022-03-31 16:41:04 +0200

    Spring boot and react project created.

---
+++ gymmembership/src/test/java/com/martoncode/gymmembership/GymmembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martoncode.gymmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
